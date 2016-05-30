a1c0dc3f54364ae36c9d29a9f7a648ea43d1d240
Marianna Veríssimo <marianna1204@gmail.com>
2016-05-30 15:07:48 -0300

    Exercicio 1042 do URI em Python

---
+++ 1042.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#Pegando os números do teclado
+entrada = raw_input().split()
+#Transformando eles em inteiro
+a = int(entrada[0])
+b = int(entrada[1])
+c = int(entrada[2])
+
+#Colocando todos em uma lista
+lista = [a,b,c]
+
+#Ordenando a lista
+lista.sort()
+
+#Mostrando os numeros ordenados
+print lista[0]
+print lista[1]
+print lista[2]
+print ""
+
+#Mostrando os números como foram digitados
+print a
+print b
+print c
