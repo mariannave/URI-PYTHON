604f292513868bab6af5edb5f8aa49cb95a278e8
Marianna Veríssimo <marianna1204@gmail.com>
2016-06-13 08:32:34 -0300

    Create uri1050.py

---
+++ uri1050.py
@@ -0,0 +1,21 @@
+entrada = int(raw_input())
+
+
+if(entrada == 61):
+	print "Brasilia"
+elif(entrada == 71):
+	print "Salvador"
+elif(entrada == 11):
+	print "Sao Paulo"
+elif(entrada == 21):
+	print "Rio de Janeiro"
+elif(entrada == 32):
+	print "Juiz de Fora"
+elif(entrada == 19):
+	print "Campinas"
+elif(entrada == 27):
+	print "Vitoria"
+elif(entrada == 31):
+	print "Belo Horizonte"
+else:
+	print "DDD nao cadastrado"
