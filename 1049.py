86798faf44bd128c9a65359039efbb7fd138deb0
Marianna Veríssimo <marianna1204@gmail.com>
2016-06-13 09:24:13 -0300

    Create 1049.py

---
+++ 1049.py
@@ -0,0 +1,30 @@
+ent1 = raw_input()
+ent2 = raw_input()
+ent3 = raw_input()
+
+if(ent1 == "vertebrado"):
+	if(ent2 == "ave"):
+		if(ent3 == "carnivoro"):
+			print "aguia"
+		else:
+			print "pomba"
+	else:
+		if(ent3 == "onivoro"):
+			print "homem"
+		else:
+			print "vaca"
+else:
+	if(ent2 == "inseto"):
+		if(ent3 == "hematofago"):
+			print "pulga"
+		else:
+			print "lagarta"
+	else:
+		if(ent3 == "hematofago"):
+			print "sanguessuga"
+		else:
+			print "minhoca"
+			
+			
+
+			
