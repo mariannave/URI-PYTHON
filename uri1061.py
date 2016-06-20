b67f1c3a576e531ca4a35faecc5f10811e3849ed
Marianna Veríssimo <marianna1204@gmail.com>
2016-06-20 08:42:38 -0300

    Create uri1061.py

---
+++ uri1061.py
@@ -0,0 +1,38 @@
+diainicio = raw_input().split()
+diainicio = int(diainicio[1])
+horainicio = raw_input().split(":")
+hinicio = int(horainicio[0])
+minicio = int(horainicio[1])
+sinicio = int(horainicio[2])
+
+diafinal = raw_input().split()
+diafinal = int(diafinal[1])
+horafinal = raw_input().split(":")
+hfinal = int(horafinal[0])
+mfinal = int(horafinal[1])
+sfinal = int(horafinal[2])
+
+dia = diafinal - diainicio
+
+hora = hfinal - hinicio
+if(hora < 0):
+	hora = 24 + hora
+	dia = dia - 1
+
+minuto = mfinal - minicio 
+if(minuto < 0):
+	minuto = 60 + minuto
+	hora = hora - 1
+	
+segundos = sfinal - sinicio
+if(segundos < 0):
+	segundos = 60 + segundos
+	minuto = minuto - 1
+
+if(dia <= 0):
+	dia = 0
+
+print "%d dia(s)" %dia
+print "%d hora(s)" %hora
+print "%d minuto(s)" %minuto
+print "%d segundo(s)" %segundos
