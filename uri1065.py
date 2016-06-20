779872455db87fc8cb15fc086e37f5877ae955ff
Marianna Veríssimo <marianna1204@gmail.com>
2016-06-20 09:17:53 -0300

    Create uri1065.py

---
+++ uri1065.py
@@ -0,0 +1,18 @@
+n1 = int(raw_input())
+n2 = int(raw_input())
+n3 = int(raw_input())
+n4 = int(raw_input())
+n5 = int(raw_input())
+
+lista =[n1,n2,n3,n4,n5]
+
+i = 0
+pares = 0
+while(i < len(lista)):
+	if(lista[i] %2 == 0):
+		pares = pares + 1
+	
+	i = i +1
+	
+	
+print "%d valores pares" %pares
