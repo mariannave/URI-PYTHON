e29edaa7027bfa6b1376e1cab81ca08face82c5e
Alexandre Melo <xandymelo@outlook.com.br>
2019-10-02 21:15:32 -0300

    Create uri1073.py

---
+++ uri1073.py
@@ -0,0 +1,11 @@
+n=int(input())
+par=0
+if n%2==1:
+    n=n-1
+    while par<n:
+        par+=2
+        print("{}^2 = {}".format(par, par * par))
+if n%2==0:
+    while par<n:
+        par+=2
+        print("{}^2 = {}".format(par, par * par))
