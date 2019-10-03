b15a313190f7409f15165111ab0df5e530a8fd0a
Alexandre Melo <xandymelo@outlook.com.br>
2019-10-02 21:16:59 -0300

    Create uri1078.py

---
+++ uri1078.py
@@ -0,0 +1,2 @@
+n=int(input())
+print("1 x {} = {}\n2 x {} = {}\n3 x {} = {}\n4 x {} = {}\n5 x {} = {}\n6 x {} = {}\n7 x {} = {}\n8 x {} = {}\n9 x {} = {}\n10 x {} = {}".format(n,n*1,n,n*2,n,n*3,n,n*4,n,n*5,n,n*6,n,n*7,n,n*8,n,n*9,n,n*10))
