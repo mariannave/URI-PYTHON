4b7ca3f48e965f46c5bc3f64e839b27f89243282
Alexandre Melo <xandymelo@outlook.com.br>
2019-10-02 21:13:28 -0300

    Create 1070.py

---
+++ uri1070.py
@@ -0,0 +1,16 @@
+x=int(input())
+
+if x%2==0:
+    print(x + 1)
+    print(x + 3)
+    print(x + 5)
+    print(x + 7)
+    print(x + 9)
+    print(x + 11)
+if x%2==1:
+    print(x)
+    print(x + 2)
+    print(x + 4)
+    print(x + 6)
+    print(x + 8)
+    print(x + 10)
