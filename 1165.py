a1c9f2331901583a9824be6879a6aab6e0e05343
Alexandre Melo <xandymelo@outlook.com.br>
2019-10-07 16:32:57 -0300

    Create 1165.py

---
+++ 1165.py
@@ -0,0 +1,17 @@
+def primo(n):
+    if (n == 2):
+        return 1
+    if (n == 0 or n == 1 or (n % 2 == 0)):
+        return 0
+    for i in range(3, int(n**(1/2)) + 2):
+        if (n % i == 0):
+            return 0
+    return 1
+quant_de_testes = int(input())
+for c in range(0,quant_de_testes):
+  numero = int(input())
+  if primo(numero):
+    print("{} eh primo".format(numero))
+  elif not primo(numero):
+    print("{} nao eh primo".format(numero))
+  
