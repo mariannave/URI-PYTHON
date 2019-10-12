87ce2846e2ca1a80d4929591ca86dd6aaa5824c5
Alexandre Melo <xandymelo@outlook.com.br>
2019-10-12 11:35:50 -0300

    Update uri1007.py

--- Iniciante - Beginner/uri1007.py
+++ Iniciante - Beginner/uri1007.py
@@ -1,8 +1,26 @@
+'''
+  Entrada:  5
+            6
+            7
+            8
+  Saída: DIFERENCA = -26
+  -----------------
+  Entrada:  0
+            0
+            7
+            8
+  Saída: DIFERENCA = -56
+  -----------------
+  Entrada:  5
+            6
+            -7
+            8
+  Saída: DIFERENCA = 86
+'''
+
 n1 = int(input())
 n2 = int(input())
 n3 = int(input())
 n4 = int(input())
-
 diferenca = (n1 * n2) - (n3 * n4)
-
 print("DIFERENCA = %d" %diferenca)
