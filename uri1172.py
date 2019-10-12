5a5f3f7973f9a5af9cbd9f0333d9605296b301be
Alexandre Melo <xandymelo@outlook.com.br>
2019-10-12 10:37:12 -0300

    Update uri1172.py

--- uri1172.py
+++ uri1172.py
@@ -6,3 +6,19 @@ for c in range(10):
   vetor.append(numero)
 for c in range(10):
   print("X[{}] = {}".format(c,int(vetor[c])))
+
+  
+  '''
+  Entrada:
+0
+-5
+63
+0
+...
+Saída:
+X[0] = 1
+X[1] = 1
+X[2] = 63
+X[3] = 1
+...
+'''
