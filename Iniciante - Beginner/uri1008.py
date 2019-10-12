4e2c9ec1c54d05564e768f0b62381475046d4123
Alexandre Melo <xandymelo@outlook.com.br>
2019-10-12 11:36:16 -0300

    Update uri1008.py

--- Iniciante - Beginner/uri1008.py
+++ Iniciante - Beginner/uri1008.py
@@ -1,8 +1,26 @@
+'''
+  Entrada:  25
+            100
+            5.50
+  Saída:  NUMBER = 25
+          SALARY = U$ 550.00
+  -----------------
+  Entrada:  1
+            200
+            20.50
+  Saída:  NUMBER = 1
+          SALARY = U$ 4100.00
+  -----------------
+  Entrada:  6
+            145
+            15.55
+  Saída:  NUMBER = 6
+          SALARY = U$ 2254.75
+'''
+
 numfunc = int(input())
 hrtrab = int(input())
 valorhr = float(input())
-
 salario = float(hrtrab * valorhr)
-
 print("NUMBER = %d" %numfunc)
 print("SALARY = U$ %0.2f" %salario)
