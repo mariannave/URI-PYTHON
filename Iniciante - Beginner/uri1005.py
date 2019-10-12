601ebae754f8bce8ce9f336103234e2f6f9f81a2
Alexandre Melo <xandymelo@outlook.com.br>
2019-10-12 11:33:58 -0300

    Update uri1005.py

--- Iniciante - Beginner/uri1005.py
+++ Iniciante - Beginner/uri1005.py
@@ -1,6 +1,20 @@
+'''
+  Entrada:  5.0
+            7.1
+  Saída: MEDIA = 6.43182
+  -----------------
+  Entrada:  0.0
+            7.1
+  Saída: MEDIA = 4.84091
+  -----------------
+  Entrada:  10.0
+            10.0
+  Saída: MEDIA = 10.00000
+'''
+
 nota1 = float(input())
 nota2 = float(input())
 
 media = (((nota1 * 3.5) + (nota2 * 7.5)) / 11)
 
-print("MEDIA = %0.5f" %media)
+print("MEDIA = %0.5f" %media
