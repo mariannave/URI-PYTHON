0da581b7fc5214de9b1775ba80d00c6514c37f9f
Alexandre Melo <xandymelo@outlook.com.br>
2019-10-12 11:39:59 -0300

    Update uri1013.py

--- Iniciante - Beginner/uri1013.py
+++ Iniciante - Beginner/uri1013.py
@@ -1,11 +1,15 @@
+'''
+  Entrada: 7 14 106
+  Saída: 106 eh o maior
+  -----------------
+  Entrada: 217 14 6
+  Saída: 217 eh o maior
+'''
+
 import math
 
 valor = input().split(" ")
-
 a, b, c = valor
-
 maior = (int(a) + int(b) + abs(int(a) - int(b)))  / 2
 resultado = (int(maior) + int(c) + abs(int(maior) - int(c)))/2
-
 print("%d eh o maior" %resultado)
-
