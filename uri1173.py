300d5a63e593bbf99286daaf71f6a1f0d619740d
Alexandre Melo <xandymelo@outlook.com.br>
2019-10-12 10:38:04 -0300

    Update uri1173.py

--- uri1173.py
+++ uri1173.py
@@ -3,3 +3,14 @@ for c in range(10):
   print("N[{}] = {}".format(c,primeiro_valor))
   primeiro_valor = primeiro_valor * 2
   
+  '''
+  Entrada: 1
+  
+  Saída: 
+N[0] = 1
+N[1] = 2
+N[2] = 4
+...
+
+'''
+  
