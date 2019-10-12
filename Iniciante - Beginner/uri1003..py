007e719b9ef33acbd6b140029809d705c39485e4
Alexandre Melo <xandymelo@outlook.com.br>
2019-10-12 11:32:10 -0300

    Update uri1003..py

--- Iniciante - Beginner/uri1003..py
+++ Iniciante - Beginner/uri1003..py
@@ -1,6 +1,18 @@
+'''
+  Entrada:  30
+            10
+  Saída: SOMA = 40
+  -----------------
+  Entrada:  -30
+            10
+  Saída: SOMA = -20
+  -----------------
+  Entrada:  0
+            0
+  Saída: SOMA = 0
+'''
+
 A = int(input())
 B = int(input())
-
 soma = A + B
-
 print("SOMA = %d" %soma)
