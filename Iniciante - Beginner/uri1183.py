cb2cd301815ccf0377eaadc888b7b13d335a7ca2
Alexandre Melo <xandymelo@outlook.com.br>
2019-10-18 16:09:39 -0300

    Create uri1183.py

---
+++ Iniciante - Beginner/uri1183.py
@@ -0,0 +1,25 @@
+'''
+Entradas:
+S
+1.0
+0.0
+-3.5
+2.5
+4.1
+...
+
+Saída:
+12.6
+
+'''
+soma_ou_media = input()
+soma = 0
+for i in range(12):
+  for j in range(12):
+    elem = float(input())
+    if j > i:
+      soma += elem
+if soma_ou_media == "S":
+  print("{:.1f}".format(soma))
+elif soma_ou_media == "M":
+  print("{:.1f}".format(soma / 66))
